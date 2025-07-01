0146b2b3f614032965c4bdddd262ce4d4b69ec7b
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2025-07-01 22:36:03 +0430

    Complete level1 Fully

--- level1.js
+++ level1.js
@@ -3,6 +3,31 @@
 // This level is highly insecure and is not recommended using in real-world projects
 // It's level 1, so we will increase the security step by step, level by level
 
+//Register
+app.post("/register", async (req, res) => {
+  const email = req.body.username.trim();
+  const password = req.body.password.trim();
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
+      );
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Login
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
